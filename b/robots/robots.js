27d80b6da4abb0e3c899822eb574e8f6dfab5289
Andrey Lechev <andrey.lechev@inn.ru>
2014-08-18 08:15:14 +0200

    Robots scene

--- b/robots/robots.js
+++ b/robots/robots.js
@@ -1,80 +1,44 @@
-var param1 = 1;
-var param2 = 1;
-var fight_type = Array("Ближний бой", "Средний бой", "Дальний бой");
-var speed_type = Array("Быстрое перемещение", "Среднее перемещение", "Медленное перемещение");
-var w = 0;
-var h = 0;
-
 $(document).ready(function(){
-	//----up
-	$('.pofo-robot-params1-block .pofo-robot-params-button-next').click(function(){
-		if(!$(this).hasClass('dis')){
-			param1 = param1 + 1;
-			if(param1 == 3){
-				$(this).addClass('dis');
-			}
-			par = $(this).parent();
-			$('.pofo-robot-params-button-prev', par).removeClass('dis');
-			$('.div.pofo-robot-params-txt', par).fadeOut(150);
-			$('.pofo-robot-top').removeClass('move_step1 move_step2 move_step3');
-			$('.pofo-robot-type1-tail').removeClass('move_step1 move_step2 move_step3');
-			$('.pofo-robot-top').addClass('move_step'+param1);
-			$('.pofo-robot-type1-tail').addClass('move_step'+param1);
-			$('div.pofo-robot-params-txt', par).html(fight_type[param1-1]);
-			$('div.pofo-robot-params-txt', par).fadeIn(150);
-		}
-	});
-	$('.pofo-robot-params1-block .pofo-robot-params-button-prev').click(function(){
-		if(!$(this).hasClass('dis')){
-			param1 = param1 - 1;
-			if(param1 == 1){
-				$(this).addClass('dis');
-			}
-			par = $(this).parent();
-			$('.pofo-robot-params-button-next', par).removeClass('dis');
-			$('div.pofo-robot-params-txt', par).fadeOut(150);
-			$('.pofo-robot-top').removeClass('move_step1 move_step2 move_step3');
-			$('.pofo-robot-type1-tail').removeClass('move_step1 move_step2 move_step3');
-			$('.pofo-robot-top').addClass('move_step'+param1);
-			$('.pofo-robot-type1-tail').addClass('move_step'+param1);
-			$('div.pofo-robot-params-txt', par).html(fight_type[param1-1]);
-			$('div.pofo-robot-params-txt', par).fadeIn(150);
 
-		}
-	});
-	//----\up
+	var robotsPosition = { 'chest': 3, 'legs': 3 },
+		robotsContainersSelector = { 'chest': '.pofo-robots--container-chests, .pofo-robots--container-tails', 'legs': '.pofo-robots--container-legs' },
+		robotsTypesDescriptions = {
+			'chest': ['Дальний бой', 'Средний бой', 'Ближний бой'],
+			'legs': ['Медленное перемещение', 'Среднее перемещение', 'Быстрое перемещение']
+		};
 
-	//----down
-	$('.pofo-robot-params2-block .pofo-robot-params-button-next').click(function(){
-		if(!$(this).hasClass('dis')){
-			param2 = param2 + 1;
-			if(param2 == 3){
-				$(this).addClass('dis');
-			}
-			par = $(this).parent();
-			$('.pofo-robot-params-button-prev', par).removeClass('dis');
-			$('div.pofo-robot-params-txt', par).fadeOut(150);
-			$('.pofo-robot-foot').removeClass('move_step1 move_step2 move_step3');
-			$('.pofo-robot-foot').addClass('move_step'+param2);
-			$('div.pofo-robot-params-txt', par).html(fight_type[param2-1]);
-			$('div.pofo-robot-params-txt', par).fadeIn(150);
+	$('.pofo-robots--arrow').on('click', function() {
+		// check the type of section moving
+		var parts = ($(this).hasClass('pofo-robots--arrow-chests'))? 'chest' : 'legs';
+		
+		// save current value
+		var currentPosition = robotsPosition[parts];
+
+		// get next value
+		if ($(this).hasClass('pofo-robots--arrow-left')) {
+			// next
+			if (robotsPosition[parts] == 3) return;
+			robotsPosition[parts]++;
+		} else {
+			// prev
+			if (robotsPosition[parts] == 1) return;
+			robotsPosition[parts]--;
 		}
+		
+		// switch classes (move)
+		$(robotsContainersSelector[parts])
+			.removeClass('pofo-robots--container-step-' + currentPosition)
+			.addClass('pofo-robots--container-step-' + robotsPosition[parts]);
+
+		// get parent
+		var $parent = $(this).closest('.pofo-robots--controlsGroup');
+		
+		// enable/disable arrows
+		$parent.find('.pofo-robots--arrow-left').toggleClass('pofo-robots--arrow-disabled', (robotsPosition[parts] == 3));
+		$parent.find('.pofo-robots--arrow-right').toggleClass('pofo-robots--arrow-disabled', (robotsPosition[parts] == 1));
+
+		// replace texts
+		$parent.find('.pofo-robots--controlsState').html(robotsTypesDescriptions[parts][robotsPosition[parts] - 1]);
 	});
-	$('.pofo-robot-params2-block .pofo-robot-params-button-prev').click(function(){
-		if(!$(this).hasClass('dis')){
-			param2 = param2 - 1;
-			if(param2 == 1){
-				$(this).addClass('dis');
-			}
-			par = $(this).parent();
-			$('.pofo-robot-params-button-next', par).removeClass('dis');
-			$('div.pofo-robot-params-txt', par).fadeOut(150);
-			$('.pofo-robot-foot').removeClass('move_step1 move_step2 move_step3');
-			$('.pofo-robot-foot').addClass('move_step'+param2);
-			$('div.pofo-robot-params-txt', par).html(fight_type[param2-1]);
-			$('div.pofo-robot-params-txt', par).fadeIn(150);
-		}
-	});
-	//----\down
 
 });
